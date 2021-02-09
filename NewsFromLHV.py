c15b53a403b81e65ea27f013c782dc73ffbd62c6
Rikuklane <Rikuklane@gmail.com>
2021-02-09 10:56:28 +0200

    added textfiles for each news page

--- NewsFromLHV.py
+++ NewsFromLHV.py
@@ -3,31 +3,39 @@ from bs4 import BeautifulSoup as Soup
 
 
 # Function for grabbing news titles from different news pages through LHV site
-def news_from_lhv(news_page_name, page_url, pages):  # pages stands for 20 titles per page starting from the latest
-    print(news_page_name, "\n")
+def news_from_lhv(news_page_name, page_url, pages, textfile):  # pages stands for 20 titles per page starting from the latest
+    
+    # opening up textfile
+    with open(textfile, "w", encoding="UTF-8") as f:
+    
+        heading = (news_page_name + "\n\n")
+        f.write(heading)
 
-    # loop for checking titles on multiple pages
-    for page in range(pages):
-        if page != 0:
-            page_url = page_url[:-1]+str(page)
+        # loop for checking titles on multiple pages
+        for page in range(pages):
+            if page != 0:
+                page_url = page_url[:-1]+str(page)
 
-        # opening up connection, grabbing the page
-        url_client = u_open(page_url)
-        page_html = url_client.read()
-        url_client.close()
+            # opening up connection, grabbing the page
+            url_client = u_open(page_url)
+            page_html = url_client.read()
+            url_client.close()
 
-        # html parsing
-        page_soup = Soup(page_html, "html.parser")
+            # html parsing
+            page_soup = Soup(page_html, "html.parser")
 
-        # grab each title, link and date of publishing on news page
-        containers = page_soup.findAll("a", {"class": "title"})
-        dates = page_soup.findAll("td", {"class": "date right"})
+            # grab each title, link and date of publishing on news page
+            containers = page_soup.findAll("a", {"class": "title"})
+            dates = page_soup.findAll("td", {"class": "date right"})
 
-        for container, date in zip(containers, dates):
-            link = container["href"]  # reference for full article
-            title = container.text.strip()  # title of article
-            date = date.text.strip()
-            print(title, "\t", date, "\n", link, "\n")
+            for container, date in zip(containers, dates):
+                link = container["href"]  # reference for full article
+                title = container.text.strip()  # title of article
+                date = date.text.strip().split("\r\n   ")
+                print(date)
+                f.writelines(date)
+                news = ("\n", title, "\n", link, "\n\n")
+                f.writelines(news)
 
 
 # variable for the amount of pages scraped
@@ -42,9 +50,9 @@ arileht_url = "https://fp.lhv.ee/news/delfinews/0"
 balti_borsid_url = "https://fp.lhv.ee/news/omx/0"
 
 # Using the functions to extract information
-news_from_lhv("Äripäev", aripaev_url, my_pages)
-news_from_lhv("Eesti Rahvus Ringhääling", ERR_url, my_pages)
-news_from_lhv("Rahageenius", rahageenius_url, my_pages)
-news_from_lhv("Postimees", postimees_url, my_pages)
-news_from_lhv("Ärileht", arileht_url, my_pages)
-news_from_lhv("Balti Börsid", balti_borsid_url, my_pages)
+news_from_lhv("Äripäev", aripaev_url, my_pages, "Aripaev.txt")
+news_from_lhv("Eesti Rahvus Ringhääling", ERR_url, my_pages, "ERR.txt")
+news_from_lhv("Rahageenius", rahageenius_url, my_pages, "Rahageenius.txt")
+news_from_lhv("Postimees", postimees_url, my_pages, "Postimees.txt")
+news_from_lhv("Ärileht", arileht_url, my_pages, "Arileht.txt")
+news_from_lhv("Balti Börsid", balti_borsid_url, my_pages, "Balti_Borsid.txt")
